20da3c26242007cd16075574f62e7d7aea40d542
114143 <142682333+114143@users.noreply.github.com>
2023-09-06 12:07:17 -0300

    Add files via upload

---
+++ CARRERASAPP/CARRERASAPP/Presentacion/FrmNuevaCarrera.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace CARRERASAPP.Presentacion
+{
+    partial class FrmNuevaCarrera
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNuevaCarrera = new System.Windows.Forms.Label();
+            this.lblNombreCarrera = new System.Windows.Forms.Label();
+            this.txtNombreCarrera = new System.Windows.Forms.TextBox();
+            this.lblDatosMateria = new System.Windows.Forms.Label();
+            this.lblMaterias = new System.Windows.Forms.Label();
+            this.cboMaterias = new System.Windows.Forms.ComboBox();
+            this.lblAnioCursado = new System.Windows.Forms.Label();
+            this.txtAnioCursado = new System.Windows.Forms.TextBox();
+            this.rbnPrimerCuatrimestre = new System.Windows.Forms.RadioButton();
+            this.rbnSegundoCuatrimestre = new System.Windows.Forms.RadioButton();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dgvDetalleCarrera = new System.Windows.Forms.DataGridView();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.ColIDAsignatura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColNombreAsignatura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColAnioCursado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColCuatrimestre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColAcciones = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleCarrera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNuevaCarrera
+            // 
+            this.lblNuevaCarrera.AutoSize = true;
+            this.lblNuevaCarrera.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNuevaCarrera.Location = new System.Drawing.Point(45, 27);
+            this.lblNuevaCarrera.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNuevaCarrera.Name = "lblNuevaCarrera";
+            this.lblNuevaCarrera.Size = new System.Drawing.Size(153, 25);
+            this.lblNuevaCarrera.TabIndex = 0;
+            this.lblNuevaCarrera.Text = "Nueva Carrera";
+            // 
+            // lblNombreCarrera
+            // 
+            this.lblNombreCarrera.AutoSize = true;
+            this.lblNombreCarrera.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreCarrera.Location = new System.Drawing.Point(49, 94);
+            this.lblNombreCarrera.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombreCarrera.Name = "lblNombreCarrera";
+            this.lblNombreCarrera.Size = new System.Drawing.Size(144, 20);
+            this.lblNombreCarrera.TabIndex = 1;
+            this.lblNombreCarrera.Text = "Nombre Carrera";
+            // 
+            // txtNombreCarrera
+            // 
+            this.txtNombreCarrera.Location = new System.Drawing.Point(251, 94);
+            this.txtNombreCarrera.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNombreCarrera.Name = "txtNombreCarrera";
+            this.txtNombreCarrera.Size = new System.Drawing.Size(403, 22);
+            this.txtNombreCarrera.TabIndex = 2;
+            // 
+            // lblDatosMateria
+            // 
+            this.lblDatosMateria.AutoSize = true;
+            this.lblDatosMateria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatosMateria.Location = new System.Drawing.Point(49, 145);
+            this.lblDatosMateria.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDatosMateria.Name = "lblDatosMateria";
+            this.lblDatosMateria.Size = new System.Drawing.Size(164, 20);
+            this.lblDatosMateria.TabIndex = 3;
+            this.lblDatosMateria.Text = "Datos de Materias";
+            // 
+            // lblMaterias
+            // 
+            this.lblMaterias.AutoSize = true;
+            this.lblMaterias.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaterias.Location = new System.Drawing.Point(49, 198);
+            this.lblMaterias.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMaterias.Name = "lblMaterias";
+            this.lblMaterias.Size = new System.Drawing.Size(82, 20);
+            this.lblMaterias.TabIndex = 4;
+            this.lblMaterias.Text = "Materias";
+            // 
+            // cboMaterias
+            // 
+            this.cboMaterias.FormattingEnabled = true;
+            this.cboMaterias.Location = new System.Drawing.Point(251, 192);
+            this.cboMaterias.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cboMaterias.Name = "cboMaterias";
+            this.cboMaterias.Size = new System.Drawing.Size(403, 24);
+            this.cboMaterias.TabIndex = 5;
+            // 
+            // lblAnioCursado
+            // 
+            this.lblAnioCursado.AutoSize = true;
+            this.lblAnioCursado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnioCursado.Location = new System.Drawing.Point(49, 238);
+            this.lblAnioCursado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAnioCursado.Name = "lblAnioCursado";
+            this.lblAnioCursado.Size = new System.Drawing.Size(143, 20);
+            this.lblAnioCursado.TabIndex = 6;
+            this.lblAnioCursado.Text = "Año de Cursado";
+            // 
+            // txtAnioCursado
+            // 
+            this.txtAnioCursado.Location = new System.Drawing.Point(251, 238);
+            this.txtAnioCursado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAnioCursado.Name = "txtAnioCursado";
+            this.txtAnioCursado.Size = new System.Drawing.Size(403, 22);
+            this.txtAnioCursado.TabIndex = 7;
+            // 
+            // rbnPrimerCuatrimestre
+            // 
+            this.rbnPrimerCuatrimestre.AutoSize = true;
+            this.rbnPrimerCuatrimestre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbnPrimerCuatrimestre.Location = new System.Drawing.Point(663, 192);
+            this.rbnPrimerCuatrimestre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbnPrimerCuatrimestre.Name = "rbnPrimerCuatrimestre";
+            this.rbnPrimerCuatrimestre.Size = new System.Drawing.Size(173, 21);
+            this.rbnPrimerCuatrimestre.TabIndex = 8;
+            this.rbnPrimerCuatrimestre.TabStop = true;
+            this.rbnPrimerCuatrimestre.Text = "Primer Cuatrimestre";
+            this.rbnPrimerCuatrimestre.UseVisualStyleBackColor = true;
+            // 
+            // rbnSegundoCuatrimestre
+            // 
+            this.rbnSegundoCuatrimestre.AutoSize = true;
+            this.rbnSegundoCuatrimestre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbnSegundoCuatrimestre.Location = new System.Drawing.Point(663, 239);
+            this.rbnSegundoCuatrimestre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbnSegundoCuatrimestre.Name = "rbnSegundoCuatrimestre";
+            this.rbnSegundoCuatrimestre.Size = new System.Drawing.Size(190, 21);
+            this.rbnSegundoCuatrimestre.TabIndex = 9;
+            this.rbnSegundoCuatrimestre.TabStop = true;
+            this.rbnSegundoCuatrimestre.Text = "Segundo Cuatrimestre";
+            this.rbnSegundoCuatrimestre.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(923, 226);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(100, 28);
+            this.btnAgregar.TabIndex = 12;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // dgvDetalleCarrera
+            // 
+            this.dgvDetalleCarrera.AllowUserToAddRows = false;
+            this.dgvDetalleCarrera.AllowUserToDeleteRows = false;
+            this.dgvDetalleCarrera.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetalleCarrera.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColIDAsignatura,
+            this.ColNombreAsignatura,
+            this.ColAnioCursado,
+            this.ColCuatrimestre,
+            this.ColAcciones});
+            this.dgvDetalleCarrera.Location = new System.Drawing.Point(49, 298);
+            this.dgvDetalleCarrera.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvDetalleCarrera.Name = "dgvDetalleCarrera";
+            this.dgvDetalleCarrera.ReadOnly = true;
+            this.dgvDetalleCarrera.RowHeadersWidth = 51;
+            this.dgvDetalleCarrera.Size = new System.Drawing.Size(883, 117);
+            this.dgvDetalleCarrera.TabIndex = 13;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(432, 463);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(100, 28);
+            this.btnAceptar.TabIndex = 14;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(592, 463);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(100, 28);
+            this.btnCancelar.TabIndex = 15;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // ColIDAsignatura
+            // 
+            this.ColIDAsignatura.HeaderText = "Id_Asignatura";
+            this.ColIDAsignatura.MinimumWidth = 6;
+            this.ColIDAsignatura.Name = "ColIDAsignatura";
+            this.ColIDAsignatura.ReadOnly = true;
+            this.ColIDAsignatura.Width = 150;
+            // 
+            // ColNombreAsignatura
+            // 
+            this.ColNombreAsignatura.HeaderText = " Asignatura";
+            this.ColNombreAsignatura.MinimumWidth = 6;
+            this.ColNombreAsignatura.Name = "ColNombreAsignatura";
+            this.ColNombreAsignatura.ReadOnly = true;
+            this.ColNombreAsignatura.Width = 250;
+            // 
+            // ColAnioCursado
+            // 
+            this.ColAnioCursado.HeaderText = "Año Cursado";
+            this.ColAnioCursado.MinimumWidth = 6;
+            this.ColAnioCursado.Name = "ColAnioCursado";
+            this.ColAnioCursado.ReadOnly = true;
+            this.ColAnioCursado.Width = 150;
+            // 
+            // ColCuatrimestre
+            // 
+            this.ColCuatrimestre.HeaderText = "Cuatrimeste";
+            this.ColCuatrimestre.MinimumWidth = 6;
+            this.ColCuatrimestre.Name = "ColCuatrimestre";
+            this.ColCuatrimestre.ReadOnly = true;
+            this.ColCuatrimestre.Width = 150;
+            // 
+            // ColAcciones
+            // 
+            this.ColAcciones.HeaderText = "Acciones";
+            this.ColAcciones.MinimumWidth = 6;
+            this.ColAcciones.Name = "ColAcciones";
+            this.ColAcciones.ReadOnly = true;
+            this.ColAcciones.UseColumnTextForButtonValue = true;
+            this.ColAcciones.Width = 125;
+            // 
+            // FrmNuevaCarrera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.dgvDetalleCarrera);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.rbnSegundoCuatrimestre);
+            this.Controls.Add(this.rbnPrimerCuatrimestre);
+            this.Controls.Add(this.txtAnioCursado);
+            this.Controls.Add(this.lblAnioCursado);
+            this.Controls.Add(this.cboMaterias);
+            this.Controls.Add(this.lblMaterias);
+            this.Controls.Add(this.lblDatosMateria);
+            this.Controls.Add(this.txtNombreCarrera);
+            this.Controls.Add(this.lblNombreCarrera);
+            this.Controls.Add(this.lblNuevaCarrera);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmNuevaCarrera";
+            this.Text = "Nueva Carrera";
+            this.Load += new System.EventHandler(this.FrmNuevaCarrera_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleCarrera)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNuevaCarrera;
+        private System.Windows.Forms.Label lblNombreCarrera;
+        private System.Windows.Forms.TextBox txtNombreCarrera;
+        private System.Windows.Forms.Label lblDatosMateria;
+        private System.Windows.Forms.Label lblMaterias;
+        private System.Windows.Forms.ComboBox cboMaterias;
+        private System.Windows.Forms.Label lblAnioCursado;
+        private System.Windows.Forms.TextBox txtAnioCursado;
+        private System.Windows.Forms.RadioButton rbnPrimerCuatrimestre;
+        private System.Windows.Forms.RadioButton rbnSegundoCuatrimestre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dgvDetalleCarrera;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColIDAsignatura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColNombreAsignatura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColAnioCursado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColCuatrimestre;
+        private System.Windows.Forms.DataGridViewButtonColumn ColAcciones;
+    }
+}
